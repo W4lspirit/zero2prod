e0b5c9ccf949e5f766fca4fd5b8d7cf3dead5aee
Ismael Hajy <ismael.hajy@gmail.com>
2021-09-05 17:22:38 +0200

    fix typo

--- src/configuration.rs
+++ src/configuration.rs
@@ -39,9 +39,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT");
-    settings.merge(
-        config::File::from(configuration_directory.join(environment.as_str())).required(true),
-    )?;
+    settings
+        .merge(config::File::from(configuration_directory.join(env.as_str())).required(true))?;
     settings.merge(config::Environment::with_prefix("app").separator("__"))?;
     settings.try_into()
 }
