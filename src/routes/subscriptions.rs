b7e6a47471e2d9d9cd0a60c841e7726ff24535f1
Ismael Hajy <ismael.hajy@gmail.com>
2022-02-11 14:53:35 +0100

    Reset to chapter 05
    Signed-off-by: Ismael Hajy <ismael.hajy@gmail.com>

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -8,13 +8,15 @@ pub struct FormData {
     email: String,
     name: String,
 }
+
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument(
-    name = "Adding a new subscriber",
-    skip(form, pool),
-    fields(
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    )
+name = "Adding a new subscriber",
+skip(form, pool),
+fields(
+subscriber_email = % form.email,
+subscriber_name = % form.name
+)
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match insert_subscriber(&pool, &form).await {
@@ -22,16 +24,17 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
+
 #[tracing::instrument(
-    name = "saving new subscriber details in the database",
-    skip(form, pool)
+name = "Saving new subscriber details in the database",
+skip(form, pool)
 )]
 pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id,email,name,subscribed_at)
-        VALUES ($1,$2,$3,$4)
-        "#,
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+            "#,
         Uuid::new_v4(),
         form.email,
         form.name,
@@ -40,7 +43,7 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
     .execute(pool)
     .await
     .map_err(|e| {
-        tracing::error!("Failed to execute query :{:?}", e);
+        tracing::error!("Failed to execute query: {:?}", e);
         e
     })?;
     Ok(())
